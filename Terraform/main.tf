39420bcde64b9b433ba3ac71081c80fec931f0ac
Parchuri <pc185118@ncr.com>
2019-08-16 16:12:43 +0530

    fixing the inline script

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -140,7 +140,7 @@ resource "aws_instance" "web" {
 
     inline                      = [
       "sudo apt-get update && sudo apt-get install docker.io -y",
-      "sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl) && sudo chmod 755 ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl",
+      "sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && sudo chmod 755 ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl",
       "curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/",
       "minikube start --vm-driver=none",
       "sleep 30",
