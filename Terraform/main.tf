fcbb9fd580b23fbe8cd73d7393e7cfaa935cfa16
Parchuri <pc185118@ncr.com>
2019-08-16 15:12:14 +0530

    fixing security groups

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "allow_http" {
     # HTTP (change to whatever ports you need)
     from_port   = 80
     to_port     = 80
-    protocol    = "-1"
+    protocol    = "TCP"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = ["0.0.0.0/0"]# add a CIDR block here
