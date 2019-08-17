d83f4babfc28e9a6b86d5cfd3df796f8c96b9297
Parchuri <pc185118@ncr.com>
2019-08-17 11:51:50 +0530

    updating the terraform script

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -166,7 +166,8 @@ resource "aws_instance" "web" {
       "sudo minikube start --vm-driver=none",
       "sleep 30",
       "sudo apt-get install git -y",
-      "sudo git clone https://github.com/Anurag-30/MediaWiki.git && cd MediaWiki && cd kubernetes && sudo kubectl create -f secrets.yml -f deployment.yml -f web-service.yaml"
+      "sudo git clone https://github.com/Anurag-30/MediaWiki.git && cd MediaWiki && cd kubernetes && sudo kubectl create -f secrets.yml -f mariadb-deployment.yaml -f mariadb-svc.yaml",
+      "sudo kubectl create -f app-deployment.yml -f web-service.yaml"
 
     ]
   }
