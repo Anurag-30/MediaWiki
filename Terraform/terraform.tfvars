7bd7a988b221f323877ff214a30deedb5978a5c1
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2019-11-11 16:42:04 +0530

    ss

---
+++ Terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+VPC_CIDR  =   "10.20.0.0/16"
+app_name  =   "wikimedia"
--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,6 +1,2 @@
-variable "app_name" {
-    default = "wikimedia"
-}
-variable "VPC_CIDR" {
-    default = "10.20.0.0/16"
-}
+variable "app_name" {}
+variable "VPC_CIDR" {}
